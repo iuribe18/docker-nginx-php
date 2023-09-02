f0721269f4986c9aedf32bcb19796224cacc32f5
ivan.uribe <ivan.uribe@solem.cl>
2023-09-02 17:02:24 -0500

    host

--- code/test.php
+++ code/test.php
@@ -1,6 +1,6 @@
 <?php
 try {
-    $conn = new PDO("mysql:host=erp-db-1; dbname=my_database", "root", "root_password"); 
+    $conn = new PDO("mysql:host=db-erp-db-1; dbname=my_database", "root", "root_password"); 
     // replace db, your_database, username, password with your actual db info
     $conn->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
     echo "Connected successfully";
